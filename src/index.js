a58a13be7ce02c748d772465d333d88e76a7e534
Inna-99 <https://github.com/Inna-99/theyalow>
2019-10-17 13:59:17 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let result = Array(first.length + second.length).fill(0);
+  for (let i = first.length; i--; null) {
+    let carrier = 0;
+    for (let j = second.length; j--; null) {
+      result[1 + i + j] += carrier + first[i] * second[j];
+      carrier = Math.floor(result[1 + i + j] / 10);
+      result[1 + i + j] = result[1 + i + j] % 10;
+    }
+    result[i] += carrier;
+  }
+  return result.join("").replace(/^0*(\d)/, "$1");
 }

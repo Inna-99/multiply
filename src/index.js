48b4c6f992ee906a7aee45fc0185f965456dcb81
Inna-99 <https://github.com/Inna-99/theyalow>
2019-10-23 18:21:18 +0300

    refactor: change return

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 module.exports = function multiply(first, second) {
-
-  let result = Array(first.length + second.length).fill(0);
-      for (let i = first.length; i--; null) 
-      {
-      let carrier = 0;
-      for (let j = second.length; j--; null) {
-      result[1 + i + j] += carrier + first[i] * second[j];
-      carrier = Math.floor(result[1 + i + j] / 10);
-      result[1 + i + j] = result[1 + i + j] % 10;
-    }
-    result[i] += carrier;
-  }
-  return result.join("").replace(/^0*(\d)/, "$1");
+  const firstNumber = parseInt(first);
+  const secondNumber = parseInt(second);
+  return `${firstNumber * secondNumber}`;
 }
+
